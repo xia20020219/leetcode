37dcc3d29b4d5d2685fe59cac90daf6f5a4bbe4b
xiazhanryan <84755879+xia20020219@users.noreply.github.com>
2021-10-16 18:32:16 +0800

    0.0

--- 704-二分查找.py
+++ 704-二分查找.py
@@ -1,6 +1,6 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        if target in nums :
-            return nums.index(target)
-        else:
-            return -1
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         if target in nums :
+#             return nums.index(target)
+#         else:
+#             return -1
