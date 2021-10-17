a44215435f7ae1b5ca961e2bf7845a7660781815
xiazhanryan <84755879+xia20020219@users.noreply.github.com>
2021-10-17 17:13:59 +0800

    slow ....

---
+++ 414-第三大的數.py
@@ -0,0 +1,6 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort(reverse = True)
+        if len(nums) < 3 : return nums[0]
+        else : return nums[2]
